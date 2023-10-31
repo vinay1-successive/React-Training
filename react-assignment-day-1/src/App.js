84bf8380fb26fa9275f7d0ce8a9231bfaf405080
vinay1-successive <vinay.tomar@successive.tech>
2023-10-31 14:12:47 +0530

    Basic Commit-2

--- react-assignment-day-1/src/App.js
+++ react-assignment-day-1/src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import Question1 from "./Components/Question1";
-import Question2 from "./Components/Question2";
-import Question3 from "./Components/Question3";
-import Question4 from "./Components/Question4";
-import Question5 from "./Components/Question5";
-import Question6 from "./Components/Question6";
+import Questions from "./Components/Questions";
 function App() {
   return (
     <>
-      <Question1 />
-      <Question2 />
-      <Question3 />
-      <Question4 />
-      <Question5 />
-      <Question6 />
+      <Questions />
     </>
   );
 }
--- react-assignment-day-1/src/Components/Weather.jsx
+++ react-assignment-day-1/src/Components/Weather.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const Weather = (props) => {
   const { value } = props;
   let text = "";
